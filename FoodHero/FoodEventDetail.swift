3c5fbf1036e2fc51fb26dd10e3b3104979da691d
Lacie Fan Yuxin <lacie@velocloud.net>
2016-07-28 18:21:39 +0800

    food omg scroll

--- FoodHero/FoodEventDetail.swift
+++ FoodHero/FoodEventDetail.swift
@@ -12,7 +12,7 @@ import Alamofire
 class FoodEventDetail:UITableViewController{
     
     @IBOutlet weak var eventname: UILabel!
-    @IBOutlet weak var foodImg: UIImageView!
+
     @IBOutlet weak var location: UILabel!
     @IBOutlet weak var user: UILabel!
     @IBOutlet weak var userImg: UIImageView!
@@ -23,45 +23,18 @@ class FoodEventDetail:UITableViewController{
     var appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     var foodImages = [NSData]()
     
-    override func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
-        if scrollView.restorationIdentifier == "imgScrollView" {
-            let pageWidth:CGFloat = CGRectGetWidth(scrollView.frame)
-            let currentPage:CGFloat = floor((scrollView.contentOffset.x-pageWidth/2)/pageWidth)+1
-            var page = Int(currentPage)
-            
-            if currentPage == 0 {
-                foodImg.image = UIImage(data: foodImages[page])
-            }
-        }
-    }
+//    override func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+//        if scrollView.restorationIdentifier == "imgScrollView" {
+//            let pageWidth:CGFloat = CGRectGetWidth(scrollView.frame)
+//            let currentPage:CGFloat = floor((scrollView.contentOffset.x-pageWidth/2)/pageWidth)+1
+//            var page = Int(currentPage)
+//            
+//        }
+//    }
     
 
     
     override func viewWillAppear(animated: Bool) {
-        dispatch_async(dispatch_get_main_queue()) { 
-            let params = ["order": 0, "roomname": self.userData!["roomname"] as! String]
-            let headers = ["Content-Type": "application/json", "Authorization": "Bearer " + NSUserDefaults.standardUserDefaults().stringForKey("authToken")!]
-            
-            Alamofire.request(.POST, self.appDelegate.host + "/get-all-images", parameters: params as? [String : AnyObject], encoding: .JSON , headers: headers).responseJSON(completionHandler: { (response) in
-                
-                if let data = response.result.value {
-                    let arr = data["imgNames"] as! NSArray
-                    print(arr.count)
-                    for i in 0..<arr.count {
-                        Alamofire.request(.GET, self.appDelegate.host + "/images/" + (arr[i]["filename"] as! String)).response(completionHandler: { (request, response, data, err) in
-                            self.foodImages.append(data!)
-                            
-                            if i==0 {
-                                self.foodImg.image = UIImage(data: data!)
-                                
-                            }
-                        })
-                        
-                        
-                    }
-                }
-            })
-        }
     }
     
     override func viewDidLoad() {
@@ -76,10 +49,48 @@ class FoodEventDetail:UITableViewController{
         let btn = UIView(frame: CGRectMake(10, 10, 100, 100))
         btn.backgroundColor = UIColor.blueColor()
         
-        if(foodImages.count > 0) {
-            foodImg.image = UIImage(data: foodImages[0])
-        }
         //self.navigationController!.view.addSubview(btn)
+        
+        
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            let params = ["order": 0, "roomname": self.userData!["roomname"] as! String]
+            let headers = ["Content-Type": "application/json", "Authorization": "Bearer " + NSUserDefaults.standardUserDefaults().stringForKey("authToken")!]
+            
+            Alamofire.request(.POST, self.appDelegate.host + "/get-all-images", parameters: params as? [String : AnyObject], encoding: .JSON , headers: headers).responseJSON(completionHandler: { (response) in
+                
+                if let data = response.result.value {
+                    let arr = data["imgNames"] as! NSArray
+                    print(arr.count)
+                    
+                    for i in 0..<arr.count {
+                        Alamofire.request(.GET, self.appDelegate.host + "/images/" + (arr[i]["filename"] as! String)).response(completionHandler: { (request, response, data, err) in
+                            self.foodImages.append(data!)
+                            
+                                
+                                let newSize = CGSize(width: self.imgScrollView.bounds.height, height: self.imgScrollView.bounds.height)
+                                
+                                UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+                                let pic = UIImage(data: data!)
+                                pic?.drawInRect(CGRectMake(0, 0, newSize.width, newSize.height))
+                                let newImg = UIGraphicsGetImageFromCurrentImageContext()
+                                UIGraphicsEndImageContext()
+                            
+                                let newImgView = UIImageView(frame: CGRectMake(self.imgScrollView.bounds.width*CGFloat(i), self.imgScrollView.bounds.origin.y, self.imgScrollView.bounds.width, self.imgScrollView.bounds.height))
+                            
+                                newImgView.image = newImg
+                                self.imgScrollView.addSubview(newImgView)
+                                self.imgScrollView.delegate = self
+                                self.imgScrollView.contentSize = CGSizeMake(self.imgScrollView.bounds.width*CGFloat(arr.count), self.imgScrollView.bounds.height)
+                            
+                        })
+                        
+                        
+                    }
+                }
+            })
+        }
+
         
     }
     
